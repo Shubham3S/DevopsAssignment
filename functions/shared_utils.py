56165ad76e00ec59ba7b9244bfa44dd90a1e9086
Shubham3S <91089133+Shubham3S@users.noreply.github.com>
2025-06-28 17:09:24 +0530

    Create shared_utils.py

---
+++ functions/shared_utils.py
@@ -0,0 +1,13 @@
+# shared_utils.py
+
+def query_cosmos_db(query, param):
+    pass  # Implement Cosmos DB query
+
+def upload_to_blob_storage(path, data):
+    pass  # Implement Blob upload
+
+def read_from_cosmos_db(billing_id):
+    pass  # Implement Cosmos DB read by id
+
+def download_from_blob_storage(path):
+    pass  # Implement Blob download
